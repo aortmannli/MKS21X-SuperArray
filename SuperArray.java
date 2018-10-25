e1895f35663a2a772bf3c5033df63aa20e919e1f
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2018-10-25 17:23:52 -0400

    created a driver class + tranferred the code between accounts

---
+++ SuperArray.java
@@ -0,0 +1,60 @@
+c class SuperArray{
+	  private int size=0;
+	  private String[] data;
+
+
+	  public SuperArray(){
+	    data = new String[];
+	  }
+
+
+	  public void clear(){
+	    size=0;
+	  }
+
+
+	  public int size(){
+	    return size;
+	  }
+
+
+	  public boolean isEmpty()}{
+	    return size ==0;
+	  }
+
+
+	  public boolean add(String str){
+	    data[size()] = str;
+	    size++;
+	  }
+
+
+	  public String toString(){
+	    String out = "{";
+	    for(int i =0; i < size()-1; i+=){
+	      out += data[i]
+	      if (i != size()-1) out +=  ",";
+	    }
+	    return out;
+	  }
+
+
+	  public String get(int index){
+	    if (index < 0 || index >= size()) return null;
+	    return data[index];
+	  }
+
+
+	  public String set(int index, String str){
+	    if (index < 0 || index >= size()) return null;
+	    data[index] = str;
+	  }
+
+
+	  private void resize() {
+	    
+	  }
+
+
+	}
+
