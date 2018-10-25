da3aa08a6b3f649ce715a6d0ab2c7b20c198326e
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2018-10-25 18:15:43 -0400

    added get() test cases to Driver

--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 public class Driver{
+  // this Driver is built on Tejas' driver
   public static void main(String[] args){
     SuperArray SA = new SuperArray();
   System.out.println("Created an empty SuperArray \"SA\" with capacity 10\n\n");
@@ -10,7 +11,7 @@ public class Driver{
   System.out.println("SA's size:                         " + SA.size());
 
   /**************************************************************************/
-
+  System.out.println("Testing add and resize.");
   SA.add("Foo");
   SA.add("666");
   SA.add("Historicus");
@@ -33,5 +34,10 @@ public class Driver{
   System.out.println("SA's current value (should return {38,666,Historicus,Greetings!,beep boop,i,like,coffee,josina,peach,beep boop, unacceptable})':\n                                  " + SA);
 
   System.out.println("SA's size (should return 12):                         " + SA.size() + "\n");
+
+  /***************************************************************************/
+  System.out.println("Testing get.");
+  System.out.println("Index: 0 (should return 38)   Value:" + SA.get(0));
+  System.out.println("Index: 11 (should return unacceptable)   Value:" + SA.get(11));
   }
 }
